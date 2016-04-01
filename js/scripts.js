83246c6dc6db153cbcda8b9bcd6f6bcde4762b0c
Connor Cummings <cpccummings@gmail.com>
2016-04-01 10:40:56 -0700

    changed logic to for if loop, var counter set to be checked with  each loop iteration and will now properly output ping, pong, and pingpong statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,23 @@
 function PingPong(input){
-  //declaring my variables
-  var userInput = input.length, i = 0
-  //while these conditions are met...
-  while(userInput % 3 === 0){
-    $('#list').append('<li>' + 'ping' + '</li>');
-    i++;
-  }
-  while(userInput % 5 === 0){
-    $('#list').append('<li>' + 'pong' + '</li>');
-    i++;
-  }
-  while(userInput % 15 === 0){
-    $('#list').append('<li>' + 'pingpong' + '</li>');
-    i++;
-  }
-  while(userInput % 3 !== 0 && userInput % 5 === 0 && userInput % 15 === 0){
-    $('#list')
-    i++;
+  for(var counter = 1, userInput = input; counter <= userInput; counter++){
+    if(counter % 15 === 0){
+      $('#list').append('<li class = "pingpong">' + 'pingpong' + '</li>');
+    }else if(counter % 5 === 0){
+      $('#list').append('<li class = "pong">' + 'pong' + '</li>');
+    }else if(counter % 3 === 0){
+      $('#list').append('<li class = "pingpong">' + 'ping' + '</li>');
+    }else{
+      $('#list').append('<li>' + counter + '</li>');
+    }
   }
 }
+
 //event listener for user's choice
 $(document).ready(function(){
   $('#button').click(function(event){
     event.preventDefault();
-    console.log('event fires');
-    var userSelectedNumber = .val();
+    console.log('fires');
+    var userSelectedNumber = $('#numberChoice').val();
     PingPong(userSelectedNumber);
-
-
-
-
-
   });
 });
